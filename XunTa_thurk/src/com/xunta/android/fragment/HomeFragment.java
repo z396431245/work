06c5bd17836c5a9f1fb87c8be8512621cc8a2699
378502765 <378502765@qq.com>
2014-07-30 11:45:25 +0800

    bk---homefragment

--- XunTa_thurk/src/com/xunta/android/fragment/HomeFragment.java
+++ XunTa_thurk/src/com/xunta/android/fragment/HomeFragment.java
@@ -2,32 +2,49 @@ package com.xunta.android.fragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.xunta.android.R;
 import com.xunta.android.base.BaseFragment;
-import com.xunta.android.item.HomeHotListViewItem;
+import com.xunta.android.item.HomeFragmentListViewItem;
 
-public class HomeFragment extends BaseFragment implements OnItemClickListener {
+public class HomeFragment extends BaseFragment implements OnItemClickListener,
+		OnClickListener {
 
 	private ListView hotListView = null;
+	private ListView nearListView = null;
 	private RelativeLayout hotLayout = null;
 	private RelativeLayout nearLayout = null;
 	int count = 10;
-	private View parentView;
+	private View parentCenterView;
+	private View parentTopView;
+	private TextView titleHot, titleNear;
 
 	@Override
 	protected void initPageView() {
-		hotLayout = (RelativeLayout) parentView.findViewById(R.id.hot_layout);
-		nearLayout = (RelativeLayout) parentView.findViewById(R.id.near_layout);
-		hotListView = (ListView) parentView.findViewById(R.id.hot_list);
-		ListAdapter adapter = new ListAdapter();
-		hotListView.setAdapter(adapter);
+		titleHot = (TextView) parentTopView.findViewById(R.id.hot_title);
+		titleNear = (TextView) parentTopView.findViewById(R.id.near_title);
+		titleNear.setOnClickListener(this);
+		titleHot.setOnClickListener(this);
+		hotLayout = (RelativeLayout) parentCenterView
+				.findViewById(R.id.hot_layout);
+		nearLayout = (RelativeLayout) parentCenterView
+				.findViewById(R.id.near_layout);
+		
+		hotListView = (ListView) parentCenterView.findViewById(R.id.hot_list);
+		ListAdapter adapterHot = new ListAdapter(View.INVISIBLE);
+		hotListView.setAdapter(adapterHot);
+		
+		nearListView = (ListView) parentCenterView.findViewById(R.id.near_list);
+		ListAdapter adapterNear = new ListAdapter(View.VISIBLE);
+		nearListView.setAdapter(adapterNear);
 	}
 
 	@Override
@@ -37,15 +54,16 @@ public class HomeFragment extends BaseFragment implements OnItemClickListener {
 
 	@Override
 	protected View loadTopLayout() {
-		return getActivity().getLayoutInflater().inflate(
+		parentTopView = getActivity().getLayoutInflater().inflate(
 				R.layout.fragment_home_title, null);
+		return parentTopView;
 	}
 
 	@Override
-	protected View loadContentLayout() {
-		parentView = getActivity().getLayoutInflater().inflate(
+	protected View loadContentLayout() { 
+		parentCenterView = getActivity().getLayoutInflater().inflate(
 				R.layout.fragment_home, null);
-		return parentView;
+		return parentCenterView;
 	}
 
 	@Override
@@ -60,6 +78,13 @@ public class HomeFragment extends BaseFragment implements OnItemClickListener {
 	}
 
 	public class ListAdapter extends BaseAdapter {
+
+		// type 1:hot other:near
+		private int showDistance;
+
+		public ListAdapter(int isShowDistance) {
+			this.showDistance = isShowDistance;
+		}
 
 		@Override
 		public int getCount() {
@@ -80,19 +105,39 @@ public class HomeFragment extends BaseFragment implements OnItemClickListener {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ViewHolder holder = new ViewHolder();
 			if (convertView == null) {
-				holder.item = new HomeHotListViewItem(getActivity());
+				holder.item = new HomeFragmentListViewItem(getActivity());
 				convertView = holder.item;
 				convertView.setTag(holder);
 			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
+			
+			holder.item.setShowDistance(showDistance);
+			
 			return convertView;
 		}
 
 		private class ViewHolder {
-			HomeHotListViewItem item;
+			HomeFragmentListViewItem item;
 		}
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.hot_title:
+			hotLayout.setVisibility(View.VISIBLE);
+			nearLayout.setVisibility(View.GONE);
+			break;
+		case R.id.near_title:
+			hotLayout.setVisibility(View.GONE);
+			nearLayout.setVisibility(View.VISIBLE);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
